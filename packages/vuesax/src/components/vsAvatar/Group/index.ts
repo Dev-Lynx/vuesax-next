aacdd26bb655b3f3a4496d70e19ae47c78d31773
PrinceOwen9466 <princeowenolive@gmail.com>
2020-12-31 21:16:04 +0100

    Staged vuesax-next v1 📽

--- packages/vuesax/src/components/vsAvatar/Group/index.ts
+++
@@ -1,9 +0,0 @@
-import { App } from "vue";
-import './style.sass';
-import component from './component'
-
-component.install = (app: App) => {
-  app.component('vs-avatar-group', component)
-}
-
-export default component
--- packages/vuesax/types/component.d.ts
+++
@@ -1,8 +0,0 @@
-import { App } from 'vue';
-import { Vue } from "vue-class-component";
-
-/** ElementUI component common definition */
-export declare class VuesaxUIComponent extends Vue {
-  /** Install component into Vue */
-  static install(app: App): void
-}
